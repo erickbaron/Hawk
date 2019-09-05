5da7c8eb4b7808bc41641eb6385219c2edaa9371
JoaoVictorDF <joaovdf17@hotmail.com>
2019-09-05 08:42:30 -0300

    Feito o Repository de Cartao e Empresa, feito o Controller de Cartao e Empresa

---
+++ Hawk.Repository/Repositories/EmpresaRepository.cs
@@ -0,0 +1,51 @@
+﻿using Hawk.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hawk.Repository.Repositories
+{
+    public class EmpresaRepository : IHawkRepository<Empresa>
+    {
+        private  HawkContext context;
+
+        public EmpresaRepository(HawkContext context)
+        {
+            this.context = context;
+        }
+
+        public int Add(Empresa entity)
+        {
+            entity.RegistroAtivo = true;
+            context.Empresas.Add(entity);
+            context.SaveChanges();
+            return entity.Id;
+        }
+
+        public bool Delete(int id)
+        {
+            var empresa = context.Empresas.FirstOrDefault(t => t.Id == id);
+            empresa.RegistroAtivo = false;
+            context.Empresas.Update(empresa);
+            return context.SaveChanges() > 0;
+        }
+
+        public Empresa ObterPeloId(int id)
+        {
+            return context.Empresas.FirstOrDefault(t => t.RegistroAtivo && t.Id == id);
+        }
+
+        public List<Empresa> ObterTodos()
+        {
+            return context.Empresas.Where(t => t.RegistroAtivo).ToList();
+        }
+
+        public bool Update(Empresa entity)
+        {
+            entity.RegistroAtivo = true;
+            context.Empresas.Update(entity);
+            return context.SaveChanges() > 0;
+        }
+    }
+}
